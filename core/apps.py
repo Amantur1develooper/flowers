01995b1aea9736f7196026945f2a024dfb78816f
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-10-16 08:43:25 +0600

    one3

--- core/apps.py
+++ core/apps.py
@@ -8,11 +8,19 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN', None) != 'true':
-            return 
+        run_main = os.environ.get('RUN_MAIN')
+        if run_main == 'true' or run_main is None:
+            from .utils import send_daily_report
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(send_daily_report, 'cron', hour=8, minute=47)
+            scheduler.start()
 
-        from .utils import send_daily_report
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(send_daily_report, 'cron', hour=8, minute=30)  # Ежедневно в 9:00 утра
-        scheduler.start()
+    # def ready(self):
+    #     if os.environ.get('RUN_MAIN', None) != 'true':
+    #         return 
+
+    #     from .utils import send_daily_report
+    #     scheduler = BackgroundScheduler()
+    #     scheduler.add_job(send_daily_report, 'cron', hour=8, minute=30)  # Ежедневно в 9:00 утра
+    #     scheduler.start()
 
--- core/telegram_bot.py
+++ core/telegram_bot.py
@@ -48,37 +48,7 @@ def send_telegram_notification(message, document_path=None):
                 print(f"Error sending document to {manager.chat_id}: {e}")
     
     return success_count > 0
-# def send_telegram_notification(message):
-#     """Отправка уведомления всем активным менеджерам"""
-#     if not settings.TELEGRAM_BOT_TOKEN:
-#         return False
-    
-#     active_managers = TelegramManager.objects.filter(
-#         is_active=True,
-#         notify_orders=True
-#     )
-    
-#     if not active_managers.exists():
-#         return False
-    
-#     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
-#     success_count = 0
 
-#     for manager in active_managers:
-#         payload = {
-#             'chat_id': manager.chat_id,
-#             'text': message,
-#             'parse_mode': 'HTML'
-#         }
-        
-#         try:
-#             response = requests.post(url, json=payload)
-#             if response.status_code == 200:
-#                 success_count += 1
-#         except Exception as e:
-#             print(f"Error sending Telegram notification to {manager.chat_id}: {e}")
-    
-#     return success_count > 0
 
 
 def set_webhook():
